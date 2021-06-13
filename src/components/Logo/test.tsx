1c4c8718aadb522f9ea18912a25b238aeb1b1fe5
Pedro <pedro-gomes17@hotmail.com>
2021-06-13 12:26:20 -0300

    new testings for size's logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,4 +17,20 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a big logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem',
+      height: '5.9rem'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem',
+      height: '3.3rem'
+    })
+  })
 })
