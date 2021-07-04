ffb656a99e4bb45b9027a5d47296504e6bc621e7
Pedro <pedro-gomes17@hotmail.com>
2021-07-03 23:56:19 -0300

    new props in heading to change line color and size

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,20 +1,32 @@
 import * as S from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
+  lineColor = 'primary',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  size = 'medium'
 }: HeadingProps) => {
   return (
-    <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    <S.Wrapper
+      color={color}
+      lineColor={lineColor}
+      lineLeft={lineLeft}
+      lineBottom={lineBottom}
+      size={size}
+    >
       {children}
     </S.Wrapper>
   )
