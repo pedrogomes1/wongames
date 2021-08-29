d3e6e34f84311d74b3c2842fdb711e82f050805e
Pedro <pedro-gomes17@hotmail.com>
2021-08-29 20:35:51 -0300

    included new banner testing for render a ribbon

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,17 +3,17 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render the heading', () => {
-    const { container } = renderWithTheme(
-      <Banner
-        img={'https://source.unsplash.com/user/willianjusten/1042x580'}
-        title={'Defy death'}
-        subtitle={'<p>Play the new <strong>CrashLands</strong> season</p>'}
-        buttonLabel={'Buy now'}
-        buttonLink={'/games/defy-death'}
-      />
-    )
+    const { container } = renderWithTheme(<Banner {...props} />)
 
     expect(
       screen.getByRole('heading', { name: /Defy death/i })
@@ -26,5 +26,27 @@ describe('<Banner />', () => {
     expect(screen.getByRole('img', { name: /Defy death/i })).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render a Ribbon', () => {
+    renderWithTheme(
+      <Banner
+        {...props}
+        ribbon="My Ribbon"
+        ribbonSize="small"
+        ribbonColor="secondary"
+      />
+    )
+
+    const ribbon = screen.getByText(/My Ribbon/i)
+
+    expect(ribbon).toBeInTheDocument()
+    expect(ribbon).toHaveStyle({
+      backgroundColor: '#3CD3C1'
+    })
+    expect(ribbon).toHaveStyle({
+      height: '2.6rem',
+      fontSize: '1.2rem'
+    })
   })
 })
