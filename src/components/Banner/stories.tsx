3c897dce3dff6ddb9cd06283a1a3d2290dbbeec7
Pedro <pedro-gomes17@hotmail.com>
2021-08-29 20:33:41 -0300

    added new props on Banner stories to choose ribbon options (color, size and text)

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -11,9 +11,38 @@ export default {
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
   },
+  argTypes: {
+    ribbon: {
+      type: 'string'
+    },
+    ribbonColor: {
+      type: 'select',
+      options: ['primary', 'secondary']
+    },
+    ribbonSize: {
+      type: 'select',
+      options: ['normal', 'small']
+    }
+  },
   parameters: {
     layout: 'fullscreen'
   }
 } as Meta
 
-export const Default: Story<BannerProps> = (args) => <Banner {...args} />
+export const Default: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+)
+
+export const WithRibbon: Story<BannerProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Banner {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  ribbon: '20% off',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary'
+}
