cb606ea82b079d86d70653b5ca842dd3a5363dd3
Pedro <pedro-gomes17@hotmail.com>
2021-10-02 10:33:17 -0300

    new home component

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,45 @@
+import * as S from './styles'
+import Menu from 'components/Menu'
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+import Heading from 'components/Heading'
+
+const Home = () => {
+  return (
+    <section>
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary" color="black">
+          News
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Most Popular
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Upcoming
+        </Heading>
+      </Container>
+
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Free Games
+        </Heading>
+      </Container>
+
+      <Container>
+        <Footer />
+      </Container>
+    </section>
+  )
+}
+
+export default Home
