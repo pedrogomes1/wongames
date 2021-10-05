d612bbe9d60c2f7f9a5e55b48c358fb7a7a27534
Pedro <pedro-gomes17@hotmail.com>
2021-10-05 00:11:41 -0300

    test tweaks adding mocks

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,11 +1,26 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
+import bannerMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
 
 import Home from '.'
 
+const props = {
+  banners: bannerMock,
+  newGames: [gamesMock[0]],
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
+  upcommingHighlight: highlightMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
+  freeHighlight: highlightMock
+}
+
 describe('<Home />', () => {
   it('should render the menu and footer', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(
@@ -13,17 +28,23 @@ describe('<Home />', () => {
     ).toBeInTheDocument()
   })
   it('should render the sections', () => {
-    renderWithTheme(<Home />)
+    renderWithTheme(<Home {...props} />)
 
     expect(screen.getByRole('heading', { name: /News/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /Most Popular/i })
     ).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /Upcoming/i })
     ).toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /Free Games/i })
     ).toBeInTheDocument()
+
+    expect(screen.getAllByText(/Defy death 1/i)).toHaveLength(1)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+    expect(screen.getAllByText(/read dead is back/i)).toHaveLength(3)
   })
 })
