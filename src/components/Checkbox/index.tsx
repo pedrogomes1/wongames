cedcbc824203eea63a4a79a6a9804a7d4b3a9228
Pedro <pedro-gomes17@hotmail.com>
2021-10-07 23:09:02 -0300

    added new Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,26 @@
+import * as S from './styles'
+
+export type CheckboxProps = {
+  label?: string
+  labelFor?: string
+  labelColor?: 'white' | 'black'
+}
+
+const Checkbox = ({
+  label,
+  labelFor = '',
+  labelColor = 'white'
+}: CheckboxProps) => {
+  return (
+    <S.Wrapper>
+      <input type="checkbox" id={labelFor} />
+      {!!label && (
+        <S.Label htmlFor={labelFor} labelColor={labelColor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
+
+export default Checkbox
