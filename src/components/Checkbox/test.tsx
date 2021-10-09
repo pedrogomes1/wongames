c22efdd0b8cc33d9dc2e4a71e197c3faacd1f6eb
Pedro <pedro-gomes17@hotmail.com>
2021-10-09 14:40:16 -0300

    new accessibility test checking whether the checkbox had focus using the keyboard tab

--- src/components/Checkbox/test.tsx
+++ src/components/Checkbox/test.tsx
@@ -53,4 +53,12 @@ describe('<Checkbox />', () => {
     })
     expect(onCheck).toHaveBeenCalledWith(false)
   })
+
+  it('should be accessible with tab', async () => {
+    renderWithTheme(<Checkbox label="Checkbox" labelFor="Checkbox" />)
+
+    expect(document.body).toHaveFocus()
+    userEvent.tab()
+    expect(screen.getByLabelText(/checkbox/i)).toHaveFocus()
+  })
 })
